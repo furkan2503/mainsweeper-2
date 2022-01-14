aafb9048f664dec5ff99e35e9e870ca2d2791e4b
Furkan <fantastisk-4@hotmail.com>
2022-01-14 21:37:12 +0100

    many mines red

--- src/no/kristiania/minesweeper/MinesweeperTest.java
+++ src/no/kristiania/minesweeper/MinesweeperTest.java
@@ -7,33 +7,56 @@ import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 public class MinesweeperTest {
     @Test
     void itShouldEmptyMinefield() {
-        assertArrayEquals(new String[] {"0"},
-                displayBoard(new String[] {"."})
+        assertArrayEquals(new String[]{"0"},
+                displayBoard(new String[]{"."})
         );
     }
 
     @Test
     void itShouldDisplaySingleMin() {
         assertArrayEquals(
-                new String[] { "*" },
-                displayBoard(new String[] {"*"})
+                new String[]{"*"},
+                displayBoard(new String[]{"*"})
         );
     }
 
     @Test
     void itShouldCorrectNumberOfRows() {
         assertArrayEquals(
-                new String[] { "0", "0", "0" },
-                displayBoard(new String[] {".", ".", "."})
+                new String[]{"0", "0", "0"},
+                displayBoard(new String[]{".", ".", "."})
         );
     }
 
+    @Test
+    void itShouldDisplayCorrectNumberOfColumns() {
+        assertArrayEquals(
+                new String[]{"000"},
+                displayBoard(new String[]{"..."})
+        );
+    }
+
+    @Test
+    void itDisplaysSeveralMines() {
+        assertArrayEquals(
+                new String[]{"***", "***"},
+                displayBoard(new String[]{"***", "***"})
+        );
+    }
+
+
     private String[] displayBoard(String[] input) {
-        if (input[0].charAt(0)=='*'){
-            return new String[] {"*"};
+        if (input[0].charAt(0) == '*') {
+            return new String[]{"*"};
         }
         String[] board = new String[input.length];
-        board[0] = "0";
+        for (int row = 0; row < board.length; row++) {
+            String rowString = "";
+            for (int column = 0; column < input[row].length(); column++) {
+                rowString += "0";
+            }
+            board[row] = rowString;
+        }
         return board;
     }
 }
